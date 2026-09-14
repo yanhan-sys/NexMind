58002b14372ab561ec6dfc82790d5ea8691c320f
Yanhan Huang <542261443@qq.com>
2026-09-14 22:32:58 +0800

    Add causal mode to multi-head attention

--- src/multi_head_attention.h
+++ src/multi_head_attention.h
@@ -10,11 +10,12 @@ namespace nexmind {
 
 class MultiHeadAttention final : public Module {
 public:
-    MultiHeadAttention(std::size_t embed_dim, std::size_t num_heads, std::uint64_t seed = 42);
+    MultiHeadAttention(std::size_t embed_dim, std::size_t num_heads, std::uint64_t seed = 42, bool causal = false);
 
     std::size_t embed_dim() const noexcept { return embed_dim_; }
     std::size_t num_heads() const noexcept { return num_heads_; }
     std::size_t head_dim() const noexcept { return head_dim_; }
+    bool causal() const noexcept { return causal_; }
 
     Value forward(const Value& input) const;
 
@@ -24,6 +25,7 @@ private:
     std::size_t embed_dim_;
     std::size_t num_heads_;
     std::size_t head_dim_;
+    bool causal_;
     std::vector<Linear> query_projections_;
     std::vector<Linear> key_projections_;
     std::vector<Linear> value_projections_;
